1883a9bc4e0934858b8df610b45aa19464ddc28e
DIEGO ARENAS <arenasvega.diego@gmail.com>
2024-06-04 19:06:27 -0400

    Modificadores Parte 2

---
+++ Statements/10_count.sql
@@ -0,0 +1,5 @@
+-- Cuenta cuantas filas contiene la tabla "users"
+SELECT COUNT(*) FROM users;
+
+-- Cuenta cuantas filas contienen un dato no nulo en el campo edad de la tabla "users"
+SELECT COUNT(init_date) FROM users;
---
+++ Statements/13_in.sql
@@ -0,0 +1,2 @@
+-- Ordena todos los datos de la tabla "users" con nombre igual a Diego y Oreo
+SELECT * FROM users WHERE name IN ('Diego', 'Oreo')
